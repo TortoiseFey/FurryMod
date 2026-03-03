07ab5373c6d73d3b266ff34e472d752c47552db3
TortoiseFey <TortoiseFryDirectorOfical@yandex.ru>
2026-03-03 14:07:43 +0300

    Create HospitalBlock.java for hospital structure

---
+++ src/main/java/com/tortoisefey/furrymod/block/HospitalBlock.java
@@ -0,0 +1,11 @@
+package com.tortoisefey.furrymod.block;
+
+public class HospitalBlock {
+    // Class implementation will go here
+    
+    public HospitalBlock() {
+        // Constructor for HospitalBlock
+    }
+
+    // Other methods and properties can be added as per the requirements
+}
