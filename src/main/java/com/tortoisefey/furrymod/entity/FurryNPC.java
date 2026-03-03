fd4b6a4aa135cd83795462392fd985e42af6ebe4
TortoiseFey <TortoiseFryDirectorOfical@yandex.ru>
2026-03-03 14:07:39 +0300

    Create FurryNPC class for furry NPCs

---
+++ src/main/java/com/tortoisefey/furrymod/entity/FurryNPC.java
@@ -0,0 +1,37 @@
+package com.tortoisefey.furrymod.entity;
+
+public class FurryNPC {
+    private String name;
+    private int age;
+    private String species;
+
+    public FurryNPC(String name, int age, String species) {
+        this.name = name;
+        this.age = age;
+        this.species = species;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+}
